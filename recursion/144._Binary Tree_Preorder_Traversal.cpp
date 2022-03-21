eb53a74de07aa9b4a9f7264b4e4dae35827e02b4
MY <42468285+marytwtw18@users.noreply.github.com>
2022-03-22 00:08:13 +0800

    Create 144._Binary Tree_Preorder_Traversal.cpp

---
+++ recursion/144._Binary Tree_Preorder_Traversal.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    //middle left right
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> list;
+        if(root == NULL) return list;
+        
+        vector<int> left = preorderTraversal(root->left);
+        vector<int> right =  preorderTraversal(root->right);
+        
+        list.push_back(root->val);   //root node
+        for(int i=0;i<left.size();++i) list.push_back(left[i]); //left node
+        for(int i=0;i<right.size();++i) list.push_back(right[i]);   //right node
+        
+        return list;
+    }
+};
